a5dc5d5dd9931f5fc78b8ce670c72db5292543a4
fo-ji <fumiya1753@gmail.com>
2022-11-29 17:31:04 +0900

    set client-server connect

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3300);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      // MEMO: add production
+    ],
+  });
+  app.use(cookieParser());
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+      // value: (req: Request) => req.header('csrf-token'),
+      value: (req: Request) => req.csrfToken(),
+    })
+  );
+  await app.listen(process.env.PORT || 3300);
 }
 bootstrap();
