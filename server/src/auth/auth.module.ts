ccd014cd54ad2cbb21d6e6a06540898270134977
fo-ji <fumiya1753@gmail.com>
2022-12-03 12:07:58 +0900

    implement get login user

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaModule } from '@/prisma/prisma.module';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [PrismaModule, JwtModule.register({})],
 })
 export class AuthModule {}
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
+import type { Request } from 'express';
+import type { User } from '@prisma/client';
+
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  getLogInUser(@Req() req: Request): User | undefined {
+    return req.user;
+  }
+}
