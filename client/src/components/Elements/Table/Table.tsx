85d4fb8b16b156401e354d1cdf1f49a1c3d943a7
fo-ji <fumiya1753@gmail.com>
2022-12-15 18:33:19 +0900

    add Table

---
+++ client/src/components/Elements/Table/Table.tsx
@@ -0,0 +1,70 @@
+type TableColumn<Entry> = {
+  title: string;
+  field: keyof Entry;
+  Cell?: ({ entry }: { entry: Entry }) => React.ReactElement;
+};
+
+export type TableProps<Entry> = {
+  data: Entry[];
+  columns: Array<TableColumn<Entry>>;
+};
+
+export const Table = <Entry extends { id: string }>({
+  data,
+  columns,
+}: TableProps<Entry>): JSX.Element => {
+  if (data.length === 0) {
+    return (
+      <div className="flex h-80 flex-col items-center justify-center bg-white text-gray-500">
+        <h4>No Entries Found</h4>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  {columns.map((col, colIdx) => (
+                    <th
+                      key={colIdx}
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500"
+                    >
+                      {col.title}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((entry, entryIdx) => (
+                  <tr
+                    key={entry.id ?? entryIdx}
+                    className="odd:bg-white even:bg-gray-100"
+                  >
+                    {columns.map(({ Cell, field }, colIdx) => (
+                      <td
+                        key={colIdx}
+                        className="whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-900"
+                      >
+                        {Cell != null ? (
+                          <Cell entry={entry} />
+                        ) : (
+                          (entry[field] as string)
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
